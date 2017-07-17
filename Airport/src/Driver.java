66573ec493776f2548e4fa930c5544b691662e26
Rbl_ <lucas.vdd@outlook.fr>
2017-07-17 14:52:11 +0100

    back

--- Airport/src/Driver.java
+++ Airport/src/Driver.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("welcome fucker");
 		Scanner reader = new Scanner(System.in);
 		Planes planes = new Planes();
 		Flights flights = new Flights();
