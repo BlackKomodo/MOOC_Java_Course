fbac406e188d002f266bb234c20a6883495fc664
Rbl_ <lucas.vdd@outlook.fr>
2017-07-17 14:49:38 +0100

    1st commit

--- Airport/src/Driver.java
+++ Airport/src/Driver.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
+		System.out.println("welcome fucker");
 		Scanner reader = new Scanner(System.in);
 		Planes planes = new Planes();
 		Flights flights = new Flights();
